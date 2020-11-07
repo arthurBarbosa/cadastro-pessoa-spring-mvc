7700495111ebd778422a99a41830894c2fef9937
Arthur Barbosa <arthurbruno03@gmail.com>
2020-11-07 12:29:20 -0400

    add: search by name person

--- src/main/java/com/abcode/springboot/repository/PersonRepository.java
+++ src/main/java/com/abcode/springboot/repository/PersonRepository.java
@@ -2,10 +2,16 @@ package com.abcode.springboot.repository;
 
 import com.abcode.springboot.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 @Transactional
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    @Query("SELECT p FROM Person p WHERE p.name LIKE %?1%")
+    List<Person> findPersonByName(String name);
 }
